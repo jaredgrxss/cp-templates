68b98372e295bf000d21fc571a4c60984ad94aa2
Gross JaredJacolby <jjgross1@cs.olemiss.edu>
2023-09-30 14:50:46 -0500

    Simplified folder

---
+++ sol.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <random>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); 
+	cin.tie(0);
+	int tc = 1; 
+	cin >> tc;
+	for (int t = 0; t < tc; t++) {
+		
+	}
+	return 0;
+}
