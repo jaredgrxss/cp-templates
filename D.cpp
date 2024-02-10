a14eae948128130c6a95342ed4239b938ee83798
Jared <jaredgrossthe@gmail.com>
2024-02-10 13:57:49 -0600

    Updated template

---
+++ D.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int tc = 1; 
+	cin >> tc;
+	for (int t = 0; t < tc; t++) {
+		
+	}
+	return 0;
+}
