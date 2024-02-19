6f588b1f44ebcd3848eb289adeb0ef1dd5117358
Jared <jaredgrossthe@gmail.com>
2024-02-18 23:12:50 -0600

    Changed template

--- E.cpp
+++ E.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+
+}
+
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int tc = 1; 
 	cin >> tc;
-	for (int t = 0; t < tc; t++) {
-		
-	}
-	return 0;
+	while (tc--) solve();
 }
