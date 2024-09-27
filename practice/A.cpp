b6c975920b9d994dc4eac23bdd0133e02115f80b
Jared <jaredgrossthe@gmail.com>
2024-09-26 23:56:02 -0500

    adding structs

--- practice/A.cpp
+++ practice/A.cpp
@@ -1,61 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5;
-int n;
-int t[2 * N];
 
-void build() {
-	for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+vector<int> dsu;
+int find(int v) {
+	if (dsu[v] == v) return v;
+	return dsu[v] = find(dsu[v]);
 }
-
-int query(int p) {
-	for (p += n; p > 0; p >>= 1) {
-		if (t[p] != 0) return t[p];
-	}
-}
-
-void modify(int l, int r, int v) {
-	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-		if (l&1) t[l++] = v;
-		if (r&1) t[--r] = v;
-	}
-}
-
-struct Battle {
-	int l;
-	int r;
-	int w;
-	Battle(int l, int r, int w) {
-		this->l = l;
-		this->r = r;
-		this->w = w;
-	};
-};
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int tc = 1; 
-	cin >> tc;
+	// cin >> tc;
 	for (int tt = 0; tt < tc; tt++) {
-		cin >> n;
-		int m; cin >> m;
-		vector<Battle> A(m);
+		int n, m; 
+		cin >> n >> m;
+		dsu.resize(n + 4);
+		vector<int> ans(n + 4);
+		for (int i = 1; i <= n + 3; i++) dsu[i] = i;
 		for (int i = 0; i < m; i++) {
-			int l, r, w; 
-			cin >> l >> r >> w;
-			Battle curr(l, r, w);
-			A.push_back(curr);
+			int l, r, x; cin >> l >> r >> x;
+			int cur = find(l);
+			while (cur <= r) {
+				if (cur == x) cur++;
+				else {
+					ans[cur] = x;
+					dsu[cur] = cur + 1;
+				}
+				cur = find(cur);
+			}
 		}
-		reverse(A.begin(), A.end());
-		for (int i = 0; i < m; i++) {
-			auto b = A[i];
-			modify(b.l, b.r, b.w);
-		}		
-		for (int i = 0; i < n; i++) {
-			int ans = query(i + 1);
-			if (ans = i) cout << 0 << endl;
-			else cout << ans << endl;
-		}
+		for (int i = 1; i <= n; i++) cout << ans[i] << ' ';
+		cout << endl;
 	}
 }
--- practice/E.cpp
+++ practice/E.cpp
@@ -6,7 +6,7 @@ int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int tc = 1; 
 	cin >> tc;
-	for (int tt = 0; tt < tc; t++) {
+	for (int tt = 0; tt < tc; tt++) {
 		
 	}
 }
